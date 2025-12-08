a16456463d09f634b716b931d2e93f4d2835c9e3
Hai Hoang Du <duhaihoa@b-tu.de>
2025-12-08 23:13:10 +0100

    form 8.12.2025

--- Form/confirm.js
+++ Form/confirm.js
@@ -3,12 +3,37 @@ function loadData() {
     var email = document.getElementById("confirm_email");
     var phonenumber = document.getElementById("confirm_phone");
     var age = document.getElementById("confirm_age");
+    var occupation = document.getElementById("confirm_occupation")
+    var courses = document.getElementById("confirm_courses")
 
     lastname = sessionStorage.getItem("lastname");
     name.textContent = sessionStorage.firstname + " " + lastname;
     email.textContent = sessionStorage.email;
     phonenumber.textContent = sessionStorage.phonenumber;
     age.textContent = sessionStorage.age;
+    occupation.textContent = sessionStorage.occupation;
+    
+    var yourCourses = " ";
+    if (sessionStorage.logicmath == "true") {
+        yourCourses += "logicmath, ";
+    }
+    if (sessionStorage.web == "true") {
+        yourCourses += "web, ";
+    }
+    if (sessionStorage.python == "true") {
+        yourCourses += "python, ";
+    }
+    if (sessionStorage.datascience == "true") {
+        yourCourses += "datascience, ";
+    }
+    if (sessionStorage.game == "true") {
+        yourCourses += "game, ";
+    }
+    if (sessionStorage.ai == "true") {
+        yourCourses += "ai, ";
+    }
+    yourCourses = yourCourses.substring(0, yourCourses.length - 2);
+    courses.textContent = yourCourses;
 }
 
 function cancelButton() {
@@ -17,10 +42,8 @@ function cancelButton() {
 
 function init() {
     loadData();
-
     var cancel = document.getElementById("cancelButton");
     cancel.onclick = cancelButton;
-
 }
 
 window.onload = init;
